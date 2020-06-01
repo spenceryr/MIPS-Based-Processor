d31e4d2148cd25e45c67bc3b812d41dd3374805e
Sammy Gill <sammygill98@gmail.com>
2020-05-31 17:43:11 -0700

    program 3 done, need to integrate into cpu and debug/test

--- our_processor/program3.asm
+++ our_processor/program3.asm
@@ -44,7 +44,7 @@ bit_loop:
   shl $r4, 5      # r4 should hold 160 at this point
   load $r2, $r4   # r2 should hold the pattern
   subr $r1, $r2
-  bneqz bit_loop_done     # if the data does not match the pattern, go to end of loop
+  bneqz after_bit_match     # if the data does not match the pattern, go to end of loop
 
   subr $r4, $r4
   addi $r4, 4     # r4 should hold 4 at this point
@@ -52,7 +52,7 @@ bit_loop:
   addi $r4, 3     # r4 should hold 7 at this point
   load $r2, $r4   # load in WITHIN_BOUNDS_MASK
   and $r1, $r2
-  bneqz after_pattern_match
+  bneqz inc_anywhere_counter
 
   # increment byteCounter and set foundWithinByte to 1
   subr $r4, $r4
@@ -99,9 +99,119 @@ after_pattern_match:
   load $r1, $r4       # r1 should hold foundWithinByte
   subi $r1, 1
   bneqz after_byte_checkk
-  store $r1, $r4
+  store $r1, $r4    # set foundWithinByte to 0
   subi $r4, 7       # r4 should hold 4
   subi $r4, 2       # r4 should hold 2
-  
-  after_byte_check:
+  load $r1, $r4     # inc numByte
+  addi $r1, 1
+  store $r1, $r4
 
+
+after_byte_check:
+  subr $r4, $r4 
+  addi $r4, 6      # r4 should hold 6
+  load $r1, $r4    # inc bytePointer to the next data byte
+  addi $r1, 1
+  store $r1, $r4
+
+  # inc i and check if we need to loop again
+  subi $r4 3        # r4 should now point to 3
+  load $r1, $r4
+  addi $r1, 1
+  store $r1, $r4
+  subi $r1, 7
+  subi $r1, 7
+  subi $r1, 7
+  subi $r1, 7
+  subi $r1, 3
+  bneqz p3_main
+
+# do pattern check over only the last byte
+last_byte:
+  subr $r1, $r1
+  subr $r2, $r2
+  subr $r3, $r3 
+  subr $r4, $r4
+  addi $r4, 4     # init j to 0
+  store $r4, $r4
+  addi $r4, 2     # r4 should hold 6 at this point
+  load $r3, $r4   # r3 should hold address of the last byte of data
+  addi $r4, 6     # r4 should hold 9 at this point
+  store $r1, $r4
+
+last_byte_bit_loop:
+  subr $r4, $r4
+  addi $r4, 7     # r4 should hold 7
+  addi $r4, 2     # r4 should hold 2
+  load $r1, $r4   # r1 should hold data byte
+  subi $r4, 1     # r4 should hold 8
+  load $r2, $r4   # r2 should hold UPPER_FIVE_BIST_MASK
+  move $r3, $r1 
+  and $r1, $r2
+
+  # load in the pattern
+  subi $r4, 3     # r4 should hold 5 at this point
+  shl $r4, 5      # r4 should hold 160 at this point
+  load $r2, $r4   # r2 should hold the pattern
+  subr $r1, $r2
+  bneqz after_last_pattern_check
+  subr $r4, $r4
+
+  load $r1, $r4
+  addi $r1, 1
+  store $r1, $r4
+  addi $r4, 1     # r4 should hold 1 at this point
+  load $r1, $r4 
+  addi $r1, 1
+  store $r1, $r4
+  addi $r4, 7     # r4 should hold 8 at this point
+  addi $r4, 3     # r4 should hold 11 at this point
+  load $r1, $r4 
+  subr $r1, $r1
+  addi $r1, 1
+  store $r1, $r4
+
+
+after_last_pattern_check: 
+  # shift data byte and update
+  subi $r4, 2       # r4 should hold 9 at this point
+  load $r1, $r4 
+  shl $r1, 1
+  store $r1, $r4
+
+  # inc j and check if we need to loop again
+  subi $r4, 5       # r4 should hold 4 at this point
+  load $r1, $r4 
+  addi $r1, 1
+  store $r1, $r4
+  subi $r1, 5
+  bneqz last_byte_bit_loop
+
+  # check foundWithinByte and inc numBytes if needed
+  addi $r4, 7       # r4 should contain 11 at this point
+  load $r1, $r4 
+  subi $r1, 1
+  bneqz p3_done
+  subi $r4, 7       # r4 should hold 4 at this point
+  subi $r4, 2       # r4 should hold 2 at this point
+  load $r1, $r4 
+  addi $r1, 1
+  store $r1, $r4
+
+
+  # move counters to right memory locations
+  p3_done:
+    subr $r4, $r4 
+    load $r1, $r4     # wihtinByte stored in r1
+    addi $r4, 1       # r4 should contain 1
+    load $r2, $r4     # r2 should contain anywhereCounter
+    addi $r4, 1       # r4 should contain 2
+    load $r3, $r4     # r3 should contain numBytes
+
+    addi $r4, 1       # r4 should contain 3
+    shl $r4, 6        # r4 should contain 192
+    store $r1, $r4 
+    addi $r4, 1       # r4 should contain 193
+    store $r3, $r4 
+    addi $r4, 1       # r4 should contain 194
+    store $r2, $r4
