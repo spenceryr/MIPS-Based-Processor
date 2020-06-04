f94ef3ddd484e3a6179ce2b2cfc14b2aef2f9a1c
Spencer Reitman Laptop <spencerreitman@gmail.com>
2020-06-04 01:16:48 -0700

    renamed module in p3_tb

--- our_processor/p3_tb.sv
+++ our_processor/p3_tb.sv
@@ -5,13 +5,13 @@
 // This is NOT synthesizable; use for logic simulation only
 // Verilog Test Fixture created for module: TopLevel
 
-module simple_t1b;	     // Lab 17
+module p3_tb;	     // Lab 17
 
 logic clk   = 0,                 // clock source -- drives DUT input of same name
       reset = 1;	             // master reset -- drives DUT input of same name
 wire  ack;		    	         // ack -- from DUT -- done w/ program
-  
-  
+
+
 // program 3-specific variables
 logic[  7:0] cto,		       // how many bytes hold the pattern? (32 max)
              cts,		       // how many patterns in the whole string? (253 max)
@@ -29,7 +29,7 @@ logic[  7:0] mat_str[32];      // message string parsed into bytes
   );
 
 initial begin
-  
+
 // Initialize DUT's data memory
 
   #10ns for(int i=0; i<256; i++) begin
@@ -46,7 +46,7 @@ initial begin
  DUT.dp.dm.core[6] = 'd128;
  DUT.dp.dm.core[7] = 'd252;
  DUT.dp.dm.core[8] = 'd248;
- 
+
 // Initialize DUT's register file
   for(int j=0; j<4; j++)
     DUT.dp.rf.registers[j] = 8'b0;    // default -- clear it
@@ -58,26 +58,26 @@ initial begin
 // pattern we are looking for; experiment w/ various values
   pat = 5'b0000;//5'b10101;//$random;
   str2 = 0;
-  DUT.dp.dm.core[160] = {pat,3'b0};   // store in upper 5 bits 
-  
+  DUT.dp.dm.core[160] = {pat,3'b0};   // store in upper 5 bits
+
   // populate memory with random bits
   for(int i=0; i<32; i++) begin
-    // search field; experiment w/ various vales   
+    // search field; experiment w/ various vales
     mat_str[i] = ($random)>>12;//8'b01010101;// $random;
     DUT.dp.dm.core[128+i] = mat_str[i];
   end
-  
+
   // put pattern in specific locations
   mat_str[0] =  8'b00000000;
   DUT.dp.dm.core[128] = mat_str[0];
-  
-  
+
+
   // create string of bits for test bench algorithm
   for(int i = 0; i<32; i++) begin
     str2 = (str2<<8)+mat_str[i];
   end
-  
-  
+
+
   ctb = 0;
   for(int j=0; j<32; j++) begin
     if(pat==mat_str[j][4:0]) ctb++;
@@ -86,14 +86,14 @@ initial begin
     if(pat==mat_str[j][7:3]) ctb++;
   end
   cto = 0;
-  for(int j=0; j<32; j++) 
+  for(int j=0; j<32; j++)
     if((pat==mat_str[j][4:0]) | (pat==mat_str[j][5:1]) |
        (pat==mat_str[j][6:2]) | (pat==mat_str[j][7:3])) cto ++;
   cts = 0;
   for(int j=0; j<252; j++) begin
     if(pat==str2[255:251]) cts++;
 	str2 = str2<<1;
-  end        	    
+  end
   #10ns reset   = 1'b1;      // pulse request to DUT
   #10ns reset   = 1'b0;
   wait(ack);               // wait for ack from DUT
